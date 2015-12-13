b6d894a122abd75c0f23d5144a8cfe93323f2311
Zhenbang Li <zhenbang.li@epfl.ch>
2015-12-13 15:07:30 -0600

    add enlarge and shrinked painting

--- test.m
+++ test.m
@@ -5,37 +5,26 @@ f3 = 'lake_small.jpg';
 f4 = 'space_shuttle.jpg';
 f5 = 'atlantis.jpg';
 
-I = im2double(imread(f5));
+painting = 'painting.jpg';
+
+I = im2double(imread(painting));
+[ho, wo, bo] = size(I);
 figure(), imshow(I);
-%% gradient
-% method = 'sobel';
-% 
-% [Gx(:,:,1), Gy(:,:,1)] = imgradientxy(I(:,:,1), method);
-% [Gx(:,:,2), Gy(:,:,2)] = imgradientxy(I(:,:,2), method);
-% [Gx(:,:,3), Gy(:,:,3)] = imgradientxy(I(:,:,3), method);
-% 
-% Gx = sqrt(Gx(:,:,1).^2+Gx(:,:,2).^2+Gx(:,:,3).^2);
-% Gy = sqrt(Gy(:,:,1).^2+Gy(:,:,2).^2+Gy(:,:,3).^2);
-% G = Gx + Gy;
+
+%% Shrink
+Ishrink = imageResize(I, ho, wo-200);
+figure(), imshow(Ishrink);
+
+%% enlarge
+Ienlarge = imageResize(I, ho, wo+100);
+figure(), imshow(Ienlarge);
 
 %% resize
-% tic
-% for i = 1:100
-%     I = carveHorizontal(I);
-% end
-% toc
-% tic
-% for i = 1:100
-%     I = carveVertical(I);
-% end
-% toc
-
-% I = kInsertVertical(I, 100);
-
-[ho, wo, bo] = size(I);
-Iresize = imageResize(I, ho-50, wo+50);
+Iresize = imageResize(I, ho+50, wo+50);
 figure(), imshow(Iresize);
 
-% Iamp = contentAmplify(I,1.2);
-% figure(), imshow(Iamp);
+
+%% amplify
+Iamp = contentAmplify(I,1.2);
+figure(), imshow(Iamp);
 
